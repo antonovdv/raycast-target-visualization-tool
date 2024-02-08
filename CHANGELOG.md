0fa767429376e49e59dc826e185bb2e395997c2b
Dmitry Antonov <antonovdvrus@gmail.com>
2024-02-09 01:37:46 +0300

    release 1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+## 1.1.0
+
+### Features
+
+- Add raycast target counter in hierarchy
+- Repaint hierarchy window after active tool changed
+
+## 1.0.0
+
+- Basic functionality
--- Editor/HierarchyColoringRaycastTargetVisualizationTool.cs
+++ Editor/HierarchyColoringRaycastTargetVisualizationTool.cs
@@ -9,13 +9,23 @@ namespace RaycastTargetVisualizationTool.Editor
     [InitializeOnLoad]
     public class HierarchyColoringRaycastTargetVisualizationTool
     {
-        private static Texture _iconTexture;
-    
+        private static readonly Texture _iconTexture;
+        private static readonly GUIStyle _style;
+
         static HierarchyColoringRaycastTargetVisualizationTool()
         {
-            var iconContent = EditorGUIUtility.IconContent("FreeformLayoutGroup Icon", "Show Raycast Target");
-            _iconTexture = iconContent.image;
+            _iconTexture = EditorGUIUtility.IconContent("FreeformLayoutGroup Icon").image;
+            _style = new GUIStyle
+            {
+                alignment = TextAnchor.MiddleRight
+            };
             EditorApplication.hierarchyWindowItemOnGUI += HandleHierarchyWindowItemOnGUI;
+            ToolManager.activeToolChanged += OnActiveToolChanged;
+        }
+
+        private static void OnActiveToolChanged()
+        {
+            EditorApplication.RepaintHierarchyWindow();
         }
 
         private static void HandleHierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
@@ -23,18 +33,43 @@ namespace RaycastTargetVisualizationTool.Editor
             GameObject obj = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
             if (obj != null && ToolManager.activeToolType == typeof(RaycastTargetVisualizationTool))
             {
-                bool hasRaycastTarget = obj.GetComponentsInChildren<Graphic>().Any(graphic => graphic.raycastTarget);
-                if (hasRaycastTarget)
+                var graphicComponent = obj.GetComponent<Graphic>();
+                if (graphicComponent != null && graphicComponent.raycastTarget)
                 {
-                    Rect iconRect = new Rect(selectionRect)
+                    DrawIcon(selectionRect, _iconTexture);
+                }
+                else
+                {
+                    var components = obj.GetComponentsInChildren<Graphic>().Where(graphic => graphic.raycastTarget).ToArray();
+                    if (components.Length > 0)
                     {
-                        x = selectionRect.xMax - 5f,
-                        width = 16f,
-                        height = 16f
-                    };
-                    EditorGUI.DrawTextureTransparent(iconRect, _iconTexture);
+                        DrawLabel(selectionRect, components.Length);
+                    }
                 }
             }
+        }
+        
+        private static void DrawIcon(Rect selectionRect, Texture texture)
+        {
+            Rect iconRect = new Rect(selectionRect)
+            {
+                x = selectionRect.xMax - 15f,
+                width = 16f,
+                height = 16f
+            };
+            EditorGUI.DrawTextureTransparent(iconRect, texture);
+        }
+
+        private static void DrawLabel(Rect selectionRect, int count)
+        {
+            Rect iconRect = new Rect(selectionRect)
+            {
+                x = selectionRect.xMax - 31f,
+                width = 32f,
+                height = 16f
+            };
+
+            EditorGUI.LabelField(iconRect, count.ToString(), _style);
         }
     }
 }
